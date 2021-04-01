e6d2cbc77af70c9acb090bc9cba7c314b327d6e0
Trevor Fehrman <trevorfehrman@gmail.com>
2021-04-01 04:31:43 -0700

    wewpz

---
+++ schemas/testimonial.js
@@ -0,0 +1,35 @@
+export default {
+  name: 'testimonial',
+  title: 'Testimonial',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'stars',
+      title: 'Stars',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'string',
+    },
+    {
+      name: 'date',
+      title: 'Date',
+      type: 'datetime',
+    },
+  ],
+};
